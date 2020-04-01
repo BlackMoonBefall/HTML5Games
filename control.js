a81b522828a76ee5341cff109738dc7373934b55
BlackMoonBefall <42492097+BlackMoonBefall@users.noreply.github.com>
2020-04-01 11:25:45 +0800

    Tt's Where all things Starts
    Just like the others,the beginning always looks terrible

---
+++ control.js
@@ -0,0 +1,20 @@
+
+
+
+
+$(document).ready(function(){
+	$("button").mouseover(function(){
+		$(this).css("background-color","yellow")
+	})
+	
+	$("button").mouseout(function(){
+		$(this).css("background-color","pink")
+	})
+	
+  
+  
+  
+  
+  
+});
+
